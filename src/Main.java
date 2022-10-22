ba593aa0c9c38e0b342608b0134bde9181d6f632
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-22 17:13:55 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner ;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Degiskenler
+        double boy, kilo, indeks ;
+
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.print("Kilonuz (kg) : ");
+        kilo = input.nextDouble() ;
+
+        System.out.print("Boyunuz (m): ");
+        boy = input.nextDouble() ;
+
+        indeks = (kilo / (boy*boy)) ;
+        System.out.println("Beden Kütle İndeksi : " + indeks);
+
     }
 }
